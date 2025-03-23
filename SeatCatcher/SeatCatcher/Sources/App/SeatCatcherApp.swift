f054caf660b27fb94df593da4c1c7779a8648431
00yhsp <00yhsp@naver.com>
2025-03-23 23:23:21 +0900

    [#7] URL 처리 로직 메소드 분리

--- SeatCatcher/SeatCatcher/Sources/App/SeatCatcherApp.swift
+++ SeatCatcher/SeatCatcher/Sources/App/SeatCatcherApp.swift
@@ -29,23 +29,21 @@ struct SeatCatcherApp: App {
         WindowGroup {
             NavigationStack(path: $coordinator.path) {
                 coordinator.buildScene(.post)
-                    .navigationDestination(for: AppScene.self) {
-                        coordinator.buildScene($0)
-                    }
-                    .sheet(item: $coordinator.appSheet) {
-                        coordinator.buildSheet($0)
-                    }
-                    .fullScreenCover(item: $coordinator.appFullScreenCover) {
-                        coordinator.buildFullScreenCover($0)
-                    }
-                    .onOpenURL {
-                        if AuthApi.isKakaoTalkLoginUrl($0) {
-                            // SDK는 AppDelegate의 openURL 메서드에서 Bool 값을 반환하도록 설계되어 있지만,
-                            // SwiftUI App 구조에서는 해당 반환값이 필요하지 않으므로 무시합니다.
-                            _ = AuthController.handleOpenUrl(url: $0)
-                        }
-                    }
+                    .navigationDestination(for: AppScene.self) { coordinator.buildScene($0) }
+                    .sheet(item: $coordinator.appSheet) { coordinator.buildSheet($0) }
+                    .fullScreenCover(item: $coordinator.appFullScreenCover) { coordinator.buildFullScreenCover($0) }
+                    .onOpenURL { handleURL($0) }
             }
+        }
+    }
+}
+
+extension SeatCatcherApp {
+    private func handleURL(_ url: URL) {
+        if AuthApi.isKakaoTalkLoginUrl(url) {
+            // SDK는 AppDelegate의 openURL 메서드에서 Bool 값을 반환하도록 설계되어 있지만,
+            // SwiftUI App 구조에서는 해당 반환값이 필요하지 않으므로 무시합니다.
+            _ = AuthController.handleOpenUrl(url: url)
         }
     }
 }
