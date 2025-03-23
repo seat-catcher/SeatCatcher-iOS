3588bfbe40d8a54fc217e21e6eecef1edea45052
00yhsp <00yhsp@naver.com>
2025-03-23 23:20:54 +0900

    [#7] App 진입점 VStack 제거

--- SeatCatcher/SeatCatcher/Sources/App/SeatCatcherApp.swift
+++ SeatCatcher/SeatCatcher/Sources/App/SeatCatcherApp.swift
@@ -27,24 +27,24 @@ struct SeatCatcherApp: App {
 
     var body: some Scene {
         WindowGroup {
-            VStack {
-                NavigationStack(path: $coordinator.path) {
-                    coordinator.buildScene(.post)
-                        .navigationDestination(for: AppScene.self) {
-                            coordinator.buildScene($0)
+            NavigationStack(path: $coordinator.path) {
+                coordinator.buildScene(.post)
+                    .navigationDestination(for: AppScene.self) {
+                        coordinator.buildScene($0)
+                    }
+                    .sheet(item: $coordinator.appSheet) {
+                        coordinator.buildSheet($0)
+                    }
+                    .fullScreenCover(item: $coordinator.appFullScreenCover) {
+                        coordinator.buildFullScreenCover($0)
+                    }
+                    .onOpenURL {
+                        if AuthApi.isKakaoTalkLoginUrl($0) {
+                            // SDK는 AppDelegate의 openURL 메서드에서 Bool 값을 반환하도록 설계되어 있지만,
+                            // SwiftUI App 구조에서는 해당 반환값이 필요하지 않으므로 무시합니다.
+                            _ = AuthController.handleOpenUrl(url: $0)
                         }
-                        .sheet(item: $coordinator.appSheet) {
-                            coordinator.buildSheet($0)
-                        }
-                        .fullScreenCover(item: $coordinator.appFullScreenCover) {
-                            coordinator.buildFullScreenCover($0)
-                        }
-                        .onOpenURL {
-                            if AuthApi.isKakaoTalkLoginUrl($0) {
-                                _ = AuthController.handleOpenUrl(url: $0)
-                            }
-                        }
-                }
+                    }
             }
         }
     }
