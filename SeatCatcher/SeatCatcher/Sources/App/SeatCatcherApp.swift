6664b80bfc5960e78ba6679bb06804e8d6c0cbfa
00yhsp <00yhsp@naver.com>
2025-03-24 02:24:54 +0900

    [#7] 자동 로그인 / 플로우 분기 TODO 작성

--- SeatCatcher/SeatCatcher/Sources/App/SeatCatcherApp.swift
+++ SeatCatcher/SeatCatcher/Sources/App/SeatCatcherApp.swift
@@ -27,6 +27,19 @@ struct SeatCatcherApp: App {
 
     var body: some Scene {
         WindowGroup {
+            // TODO: - 자동로그인 로직 작성
+            // - UserDefaults에 로그인된 상태인지 여부 isSignedIn: Bool 저장 / 해당 값으로 플로우 분기
+            // - 앱 시작 시 액세스토큰으로 로그인 시도
+            //   - 성공 시 액세스토큰 / 리프레시토큰 키체인 저장 및 isSignedIn = true
+            //   - 401 날라오면 리프레시토큰으로 토큰 갱신 시도
+            //   - 리프레시토큰도 401 날라오면 isSignedIn = false
+
+            // TODO: - 플로우 분기
+            // - 온보딩 플로우 및 유저 플로우 구축
+            // - 로그인 여부 isSignedIn: Bool / 온보딩 여부 isOnboardingNeeded: Bool 유저디폴트 저장
+            // - 온보딩 필요 시 온보딩 플로우 첫 뷰로
+            // - 온보딩 완료했으나 로그인 상태 아닐시 온보딩 플로우 내 로그인 뷰에서 플로우 시작
+            // - 로그인 상태이면 유저 플로우로
             NavigationStack(path: $coordinator.path) {
                 coordinator.buildScene(.post)
                     .navigationDestination(for: AppScene.self) { coordinator.buildScene($0) }
