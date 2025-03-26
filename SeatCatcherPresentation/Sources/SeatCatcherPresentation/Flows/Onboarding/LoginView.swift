6c3c0d5d3177e62d896239bfd9ff2fbf66550a92
00yhsp <00yhsp@naver.com>
2025-03-26 22:08:38 +0900

    [#10] 로그인 뷰 구현

--- SeatCatcherPresentation/Sources/SeatCatcherPresentation/Flows/Onboarding/LoginView.swift
+++ SeatCatcherPresentation/Sources/SeatCatcherPresentation/Flows/Onboarding/LoginView.swift
@@ -16,28 +16,57 @@ public struct LoginView: View {
     }
 
     public var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             Spacer()
-
-            SignInWithAppleButton(
-                onRequest: viewModel.handleRequest,
-                onCompletion: viewModel.handleCompletion
-            )
-            .frame(height: 50)
-
-            Button {
-                viewModel.action(.loginWithKakaoButtonTapped)
-            } label: {
-                Image("kakaoLoginButton", bundle: .module)
-                    .resizable()
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 50)
-            }
+            LogoHeaderView()
+                .padding(.bottom, 52)
+            LoginButtonGroupView(viewModel: viewModel)
+                .padding(.bottom, 199)
         }
         .padding(.horizontal, 20)
+        .withBackground()
     }
 }
 
-//#Preview {
-//    LoginView()
-//}
+private struct LogoHeaderView: View {
+    var body: some View {
+        VStack(spacing: 23) {
+            Image(.scOnboaringLogo)
+            Image(.scTextLogo)
+        }
+    }
+}
+
+private struct LoginButtonGroupView: View {
+    let viewModel: LoginViewModel
+
+    var body: some View {
+        VStack(spacing: 12) {
+            Button {
+                viewModel.action(.loginWithKakaoButtonTapped)
+            } label: {
+                HStack(spacing: 6) {
+                    Spacer()
+                    Image(.kakaoLogo)
+                        .resizable()
+                        .frame(width: 14, height: 14)
+                    Text("카카오로 계속하기")
+                        .foregroundStyle(.black)
+                        .font(.system(size: 20, weight: .medium))
+                    Spacer()
+                }
+                .padding(.vertical, 12)
+                .background(.yellow)
+                .frame(height: 52)
+                .clipShape(.rect(cornerRadius: 8))
+            }
+            SignInWithAppleButton(
+                .continue,
+                onRequest: viewModel.handleRequest,
+                onCompletion: viewModel.handleCompletion
+            )
+            .signInWithAppleButtonStyle(.white)
+            .frame(height: 52)
+        }
+    }
+}
