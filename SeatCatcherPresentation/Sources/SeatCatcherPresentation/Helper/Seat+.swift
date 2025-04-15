2cadf0830cd5dc6a7222f51fc1edb7900b613b9a
woongaaaa <woonge0930@naver.com>
2025-04-16 04:45:24 +0900

    [#21] 좌석 이미지 리소스 매핑 구현

---
+++ SeatCatcherPresentation/Sources/SeatCatcherPresentation/Helper/Seat+.swift
@@ -0,0 +1,45 @@
+//
+//  Seat+.swift
+//  SeatCatcherPresentation
+//
+//  Created by 황채웅 on 4/16/25.
+//
+
+import SeatCatcherDomain
+import SwiftUI
+
+extension Seat {
+    
+    var image: ImageResource {
+        // 좌석 방향
+        let directionPrefix = seatDirection == .top ? "seat_top" : "seat_bottom"
+        
+        // 찜
+        if hasDibsOn {
+            return ImageResource(name: "\(directionPrefix)_dibs", bundle: .module)
+        }
+        // 빈 자리
+        if isAvailable {
+            return ImageResource(name: "\(directionPrefix)_empty", bundle: .module)
+        }
+        // 내가 앉은 자리
+        if isSeated {
+            return ImageResource(name: "\(directionPrefix)_user", bundle: .module)
+        }
+        
+        // 하차까지 시간
+        let levelSuffix: String
+        switch minutesLeft {
+        case ..<11:
+            levelSuffix = "_level_0"
+        case 11..<21:
+            levelSuffix = "_level_1"
+        default:
+            levelSuffix = "_level_2"
+        }
+        
+        // 선택 상태
+        let selectedSuffix = isSelected ? "_selected" : ""
+        return ImageResource(name: "\(directionPrefix)\(levelSuffix)\(selectedSuffix)", bundle: .module)
+    }
+}
