349d0307ae5748535162e79b0128be6f41f6d490
00yhsp <00yhsp@naver.com>
2025-05-18 16:45:29 +0900

    [#32] GetPathHistoriesResponseDTO 스키마 수정

--- SeatCatcherData/Sources/SeatCatcherData/DTOs/PathHistories/GetPathHistoriesResponseDTO.swift
+++ SeatCatcherData/Sources/SeatCatcherData/DTOs/PathHistories/GetPathHistoriesResponseDTO.swift
@@ -27,18 +27,53 @@ struct GetPathHistoriesContentsResponseDTO: ResponseDTO {
     typealias DomainModel = PathHistory
 
     static var stub: Self {
-        .init(id: 1, startStationId: 1704, startStationName: "사당", endStationId: 1715, endStationName: "당산", expectedArrivalTime: "09-23", createdDate: "09-23")
+        .init(
+            id: 1,
+            startStationId: 1704,
+            startStationName: "사당",
+            startline: "LINE_2",
+            endStationId: 1715,
+            endStationName: "당산",
+            endline: "LINE_2",
+            expectedArrivalTime: "09-23",
+            createdDate: "09-23"
+        )
     }
 
     var domainModel: PathHistory {
-        .init(id: id, line: nil, departureStationId: startStationId, departureStationName: startStationName, arrivalStationId: endStationId, arrivalStationName: endStationName, createdDate: createdDate)
+        .init(
+            id: id,
+            line: line,
+            departureStationId: startStationId,
+            departureStationName: startStationName,
+            arrivalStationId: endStationId,
+            arrivalStationName: endStationName,
+            createdDate: createdDate
+        )
+    }
+
+    var line: Int {
+        switch self.startline {
+        case "LINE_1": return 1
+        case "LINE_2": return 2
+        case "LINE_3": return 3
+        case "LINE_4": return 4
+        case "LINE_5": return 5
+        case "LINE_6": return 6
+        case "LINE_7": return 7
+        case "LINE_8": return 8
+        case "LINE_9": return 9
+        default: return 2
+        }
     }
 
     let id: Int
     let startStationId: Int
     let startStationName: String
+    let startline: String
     let endStationId: Int
     let endStationName: String
+    let endline: String
     let expectedArrivalTime: String
     let createdDate: String
 }
