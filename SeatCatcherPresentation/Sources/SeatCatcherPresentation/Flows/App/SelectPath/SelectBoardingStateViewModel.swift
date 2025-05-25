1fe13627c68791cd99c7447e07b566b5862e478a
00yhsp <00yhsp@naver.com>
2025-05-25 23:28:29 +0900

    [#35] SelectBoardingStateView / ViewModel

---
+++ SeatCatcherPresentation/Sources/SeatCatcherPresentation/Flows/App/SelectPath/SelectBoardingStateViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  SelectBoardingStateViewModel.swift
+//  SeatCatcherPresentation
+//
+//  Created by 박현수 on 5/25/25.
+//
+
+import Foundation
+import SeatCatcherCore
+
+@Observable
+public final class SelectBoardingStateViewModel: ViewModel {
+    struct State {
+        var boardingState: BoardingState?
+    }
+
+    enum Action {
+        case hasBoardButtonTapped
+        case hasNotBoardedButtonTapped
+    }
+
+    private(set) var state = State()
+
+    let coordinator: Coordinator
+
+    public init(coordinator: Coordinator) {
+        self.coordinator = coordinator
+    }
+
+    func action(_ action: Action) {
+        switch action {
+        case .hasBoardButtonTapped:
+            self.state.boardingState = .boarded
+        case .hasNotBoardedButtonTapped:
+            self.state.boardingState = .notBoarded
+        }
+    }
+
+}
