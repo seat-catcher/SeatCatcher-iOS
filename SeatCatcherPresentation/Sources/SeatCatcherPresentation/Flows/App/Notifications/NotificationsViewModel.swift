d1af8bc1ab3af5552038c33418a2b02e956246de
00yhsp <00yhsp@naver.com>
2025-06-05 02:55:06 +0900

    [#52] 마이페이지 UI 구현

--- SeatCatcherPresentation/Sources/SeatCatcherPresentation/Flows/App/Notifications/NotificationsViewModel.swift
+++ SeatCatcherPresentation/Sources/SeatCatcherPresentation/Flows/App/Notifications/NotificationsViewModel.swift
@@ -12,7 +12,8 @@ import SeatCatcherCore
 public final class NotificationsViewModel: ViewModel {
     struct State {
         var notifications: [NotificationInfo] = [
-            .init(title: "test", time: Date(), message: "test"),
+            .init(title: "즐거운 짐꾼", time: Date().addingTimeInterval(-3600), message: "즐거운 짐꾼님이 3847칸 일반구역 B에서 좌석 요청을 했어요"),
+            .init(title: "열받은 노약자", time: Date().addingTimeInterval(-1284), message: "열받은 노약자님이 9183칸 일반구역 B에서 좌석 요청을 했어요")
         ]
     }
 
